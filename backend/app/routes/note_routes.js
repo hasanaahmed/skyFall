686f44aadd3538824d24f158e1f63e3a536a8637
Hasan Ahmed <hasanahmed@invitetech.com>
2018-11-24 10:34:46 -0500

    backend stuff

---
+++ backend/app/routes/note_routes.js
@@ -0,0 +1,14 @@
+// routes/note_routes.js
+module.exports = function(app, db) {
+  const collection =
+  app.post('/notes', (req, res) => {
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occurred' });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+};
