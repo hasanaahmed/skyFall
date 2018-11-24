b36ff55cc32e11227b2c89ec0d183fa9a826f974
Hasan Ahmed <hasanahmed@invitetech.com>
2018-11-24 15:12:18 -0500

    Update UEdataScrap.py

--- analysis/UEdataScrap.py
+++ analysis/UEdataScrap.py
@@ -6,13 +6,25 @@ workbook = xlrd.open_workbook('uedata.xlsx')
 sheet = workbook.sheet_by_index(0)
 
 def getIndex(year):
+    vGood = sheet.cell(62, 10)
+    good = sheet.cell(65, 10)
+    neut = sheet.cell(68, 10)
+    bad = 3.98
     using = year - 1947
-    next = using - 30
-    firstIn = sheet.cell(using, 10)
-    secondIn = sheet.cell(next, 10)
-    index = (firstIn.value - secondIn.value) / 29
-    print("The index of UE in the %i was %0.07f" % (year, index))
-    return(index)
+    input = sheet.cell(using, 10)
+
+    print("The rate of UE in %i was %0.07f" % (year, input.value))
+
+    if input.value >= vGood.value:
+        return 4
+    elif input.value >= good.value and input.value < vGood.value:
+        return 3
+    elif input.value >= neut.value and input.value < good.value:
+        return 2
+    elif input.value >= bad and input.value < neut.value:
+        return 1
+    else:
+        return 0
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
